73ddcf1c341ae2742913b687c99b6b6486753778
Duude92 <switch4acc@gmail.com>
2025-06-12 17:12:09 +0300

    Implementation of modules loading on container created

--- src/container.ts
+++ src/container.ts
@@ -1,1 +1,25 @@
-export class Container {}
+import { IContainerOptions } from './api/IContainerOptions';
+import path from 'node:path';
+import fs from 'node:fs';
+
+export const Container = {
+  create(options?: IContainerOptions) {
+    if (options?.catalogs) {
+      options.catalogs.map((catalog) => {
+        const directory = options.baseDir;
+
+        const catalogPath = path.resolve(directory, catalog);
+        const files = fs.readdirSync(catalogPath, { withFileTypes: true });
+
+        return Promise.all(
+          files
+            .filter((file) => file.isFile())
+            .map(async (file) => {
+              const filePath = path.join(file.parentPath, file.name);
+              return await import(filePath);
+            }),
+        );
+      });
+    }
+  },
+};
