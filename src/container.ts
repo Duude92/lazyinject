a530cb628e9706f46142bb890ba6b8274aa66d92
Duude92 <switch4acc@gmail.com>
2025-06-13 13:58:54 +0300

    Updated usage of 'get' method: return as T

--- src/container.ts
+++ src/container.ts
@@ -3,9 +3,10 @@ import { ContainerRegistry } from './containerRegistry';
 
 export class Container {
   get(type: InterfaceType): unknown {
+  get<T>(type: InterfaceType): T | undefined {
     const exp = ContainerRegistry.types.get(type);
     if (!exp) return undefined;
     const e2 = new exp();
-    return e2;
+    return e2 as T;
   }
 }
