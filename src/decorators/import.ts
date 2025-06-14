6cdfc3fdd9ee504c3f5dc21d4ed1e16745b1e775
Duude92 <switch4acc@gmail.com>
2025-06-14 12:17:07 +0300

    Code cleaning

--- src/decorators/import.ts
+++ src/decorators/import.ts
@@ -3,15 +3,11 @@ import { ContainerRegistryStatic } from '../containerRegistry';
 import { ConstructorType } from '../api/ConstructorType';
 
 export const Import = (interfaceType: InterfaceType) => {
-  console.log('import1');
   return (
     target: ConstructorType,
     propertyKey: string | symbol,
     parameterIndex: number,
   ) => {
-    console.log('import', propertyKey);
-    console.log(target);
-
     ContainerRegistryStatic.setImport(target, interfaceType, parameterIndex);
     return;
   };
