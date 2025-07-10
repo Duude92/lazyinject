17d4ffad96bd6c8f0a4f2b0840b1b9774ae8e70c
Duude92 <switch4acc@gmail.com>
2025-07-10 16:14:14 +0300

    Added docs for ContainerFactory

--- src/containerFactory.ts
+++ src/containerFactory.ts
@@ -3,7 +3,14 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { Container } from './container';
 
+/**
+ * Factory to create Container object
+ */
 export class ContainerFactory {
+  /**
+   * Main function to create a new Container object with provided options
+   * @param options Provided options. See {@link IContainerOptions}
+   */
   static async create(options?: IContainerOptions): Promise<Container> {
     if (options?.catalogs) {
       options.catalogs.forEach((catalog) => {
