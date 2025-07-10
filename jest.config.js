c3d20bf6e5830208c4f4f76865f6e8582a72fd8a
Duude92 <switch4acc@gmail.com>
2025-07-10 20:24:39 +0300

    Fix rootDir to enable coverage

--- jest.config.js
+++ jest.config.js
@@ -124,7 +124,7 @@ const config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: "tests",
+  // rootDir: "tests",
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
