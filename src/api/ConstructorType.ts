bc853f857df28a505a8f414b99616dd895bef380
Duude92 <switch4acc@gmail.com>
2025-07-10 12:01:05 +0300

    Added object type, and typeguard for ConstructorType to have ability to resolve exported objects

--- src/api/ConstructorType.ts
+++ src/api/ConstructorType.ts
@@ -1,1 +1,8 @@
 export type ConstructorType<T = unknown> = new (...args: unknown[]) => T;
+export type ExportedType<T = unknown> = ConstructorType<T> | object;
+export const isConstructorType = (
+  inputObject: unknown,
+): inputObject is ConstructorType =>
+  typeof inputObject === 'function' &&
+  !!inputObject.prototype &&
+  inputObject.prototype.constructor === inputObject;
