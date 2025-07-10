96dba12990fd10e935eb135ef59a297570aeeadc
Duude92 <switch4acc@gmail.com>
2025-07-10 16:32:09 +0300

    Added docs for export decorators/function

--- src/decorators/export.ts
+++ src/decorators/export.ts
@@ -2,12 +2,21 @@ import { ContainerRegistryStatic } from '../containerRegistry';
 import { InterfaceType } from '../api/interfaceType';
 import { ConstructorType } from '../api/ConstructorType';
 
+/**
+ * Allows a class constructor to be registered as an implementation of a specified {@link InterfaceType}
+ * @param interfaceType {@link InterfaceType} identifier under which given constructor function would be registered
+ * @constructor {@link ConstructorType} function, which refers to class constructor
+ */
 export const Export = (interfaceType: InterfaceType) => {
   return (constructor: ConstructorType) => {
     ContainerRegistryStatic.setExport(interfaceType, constructor);
   };
 };
-
+/**
+ * Allows the registration of an object instance under a given {@link InterfaceType}
+ * @param object Plain object, which would be registered as {@link InterfaceType} identifier
+ * @param interfaceType {@link InterfaceType} identifier under which given object would be registered
+ */
 export const $ExportObject  = (object:object, interfaceType: InterfaceType) =>{
   ContainerRegistryStatic.setExport(interfaceType, object);
 }
