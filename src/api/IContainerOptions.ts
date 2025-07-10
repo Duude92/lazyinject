460bf6930caac3677ab16568d312d7fcb4fb768d
Duude92 <switch4acc@gmail.com>
2025-07-10 16:21:01 +0300

    Added docs for IContainerOptions

--- src/api/IContainerOptions.ts
+++ src/api/IContainerOptions.ts
@@ -1,3 +1,15 @@
+/**
+ * Specifies configuration options for DI Container
+ * @param baseDir Specifies base directory of project source
+ * @param catalogs Directories to include for resolving @Import or @Export dependencies
+ * @param recursive Specifies whether container should resolve catalog dependencies recursively
+ * @example
+ * const options = {
+ *   baseDir: __dirname,
+ *   catalogs: ['.'],
+ *   recursive: true
+ * }
+ */
 export interface IContainerOptions {
   baseDir: string;
   catalogs?: string[];
