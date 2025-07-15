be1adf432a18541dc966099da958089d3adb835d
Duude92 <switch4acc@gmail.com>
2025-07-15 10:30:59 +0300

    Load both configs, from code && from config file

--- src/containerFactory.ts
+++ src/containerFactory.ts
@@ -2,7 +2,8 @@ import { IContainerOptions } from './api/IContainerOptions';
 import path from 'node:path';
 import fs from 'node:fs';
 import { Container } from './container';
-import dynamicImport from "./dynamicImport";
+import dynamicImport from './dynamicImport';
+import findRoot from './findRoot';
 
 /**
  * Factory to create Container object
@@ -14,20 +15,35 @@ export class ContainerFactory {
    */
   static async create(options?: IContainerOptions): Promise<Container> {
     if (options?.catalogs) {
-      options.catalogs.forEach((catalog) => {
-        const directory = options.baseDir;
-
-        const catalogPath = path.resolve(directory, catalog);
-        const files = fs
-          .readdirSync(catalogPath, { withFileTypes: true, recursive: options.recursive })
-          .filter((file) => file.isFile());
-
-        files.forEach(async (file) => {
-          const filePath = path.join(file.parentPath, file.name);
-          await dynamicImport(filePath);
-        });
-      });
+      this.loadModules(options);
     }
+    const root = findRoot(require.main!.path);
+    if (root) {
+      const configFile = path.join(root, 'lazyinject.config.js');
+      const lazyConfig = await dynamicImport(configFile);
+      const newOptions = {
+        baseDir: root,
+        catalogs: lazyConfig.catalogs,
+      };
+      this.loadModules(newOptions);
+    }
+
     return new Container();
   }
+
+  private static loadModules = (options: IContainerOptions) => {
+    options.catalogs!.forEach((catalog) => {
+      const directory = options.baseDir;
+
+      const catalogPath = path.resolve(directory, catalog);
+      const files = fs
+        .readdirSync(catalogPath, { withFileTypes: true, recursive: options.recursive })
+        .filter((file) => file.isFile());
+
+      files.forEach(async (file) => {
+        const filePath = path.join(file.parentPath, file.name);
+        await dynamicImport(filePath);
+      });
+    });
+  };
 }
