dd28f2b52b2dba2704e4d3ce0dbf43195d68856a
Duude92 <switch4acc@gmail.com>
2025-07-16 11:18:50 +0300

    Detach modules resolve logic

--- src/containerFactory.ts
+++ src/containerFactory.ts
@@ -1,9 +1,6 @@
 import { IContainerOptions } from './api/IContainerOptions';
-import path from 'node:path';
-import fs from 'node:fs';
 import { Container } from './container';
-import dynamicImport from './dynamicImport';
-import findRoot from './findRoot';
+import { resolveAndLoadModules } from './moduleLoader';
 
 /**
  * Factory to create Container object
@@ -14,36 +11,8 @@ export class ContainerFactory {
    * @param options Provided options. See {@link IContainerOptions}
    */
   static async create(options?: IContainerOptions): Promise<Container> {
-    if (options?.catalogs) {
-      this.loadModules(options);
-    }
-    const root = findRoot(require.main!.path);
-    if (root) {
-      const configFile = path.join(root, 'lazyinject.config.js');
-      const lazyConfig = await dynamicImport(configFile);
-      const newOptions = {
-        baseDir: root,
-        catalogs: lazyConfig.catalogs,
-      };
-      this.loadModules(newOptions);
-    }
+    await resolveAndLoadModules(options);
 
     return new Container();
   }
-
-  private static loadModules = (options: IContainerOptions) => {
-    options.catalogs!.forEach((catalog) => {
-      const directory = options.baseDir;
-
-      const catalogPath = path.resolve(directory, catalog);
-      const files = fs
-        .readdirSync(catalogPath, { withFileTypes: true, recursive: options.recursive })
-        .filter((file) => file.isFile());
-
-      files.forEach(async (file) => {
-        const filePath = path.join(file.parentPath, file.name);
-        await dynamicImport(filePath);
-      });
-    });
-  };
 }
---
+++ src/moduleLoader.ts
@@ -0,0 +1,35 @@
+import { IContainerOptions } from './api/IContainerOptions';
+import path from 'node:path';
+import fs from 'node:fs';
+import dynamicImport from './dynamicImport';
+
+const loadModules = (options: IContainerOptions) => {
+  options.catalogs!.forEach((catalog) => {
+    const directory = options.baseDir;
+
+    const catalogPath = path.resolve(directory, catalog);
+    const files = fs
+      .readdirSync(catalogPath, { withFileTypes: true, recursive: options.recursive })
+      .filter((file) => file.isFile());
+
+    files.forEach(async (file) => {
+      const filePath = path.join(file.parentPath, file.name);
+      await dynamicImport(filePath);
+    });
+  });
+};
+export const resolveAndLoadModules = async (options?: IContainerOptions) => {
+  if (options?.catalogs) {
+    loadModules(options);
+  }
+  const root = process.cwd();
+  if (root) {
+    const configFile = path.join(root, 'lazyinject.config.js');
+    const lazyConfig = await dynamicImport(configFile);
+    const newOptions = {
+      baseDir: root,
+      catalogs: lazyConfig.catalogs,
+    };
+    loadModules(newOptions);
+  }
+};
