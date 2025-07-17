fed5b05a3621d097b0a63c0bae8d1c586f299617
Duude92 <switch4acc@gmail.com>
2025-07-17 11:46:25 +0300

    Update ConstructorType, so I would be able to use it with strict mode

--- src/api/ConstructorType.ts
+++ src/api/ConstructorType.ts
@@ -1,11 +1,11 @@
 /**
  * Defines type for constructor function
  */
-export type ConstructorType<T = unknown> = new (...args: unknown[]) => T;
+export type ConstructorType<T = object> = new (...args: any[]) => T;
 /**
  * Defines type for @Export / $ExportObject functions
  */
-export type ExportedType<T = unknown> = ConstructorType<T> | object;
+export type ExportedType<T = object> = ConstructorType<T> | object;
 /**
  * Checks if inputObject is of Constructor type function
  * @param inputObject Object to check
