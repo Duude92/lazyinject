6952cc5a4398e85020096d73fcffad4e0f942572
Duude92 <switch4acc@gmail.com>
2025-07-17 11:45:49 +0300

    Update rules, so I could use any as rest args

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  globalIgnores([
-    "dist/*",
-    "samples/*",
-    "jest.config.js"
-  ])
+  globalIgnores(['dist/*', 'samples/*', 'jest.config.js']),
+  {
+    rules: {
+      ignoreRestArgs: true,
+    },
+  },
 ]);
