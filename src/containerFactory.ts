d97e0296b6419fc995b0059a960478d116cca06d
Duude92 <switch4acc@gmail.com>
2025-07-18 14:50:41 +0300

    Update container api to work with multi-container system

--- src/containerFactory.ts
+++ src/containerFactory.ts
@@ -1,17 +1,21 @@
 import { StandaloneContainerOptions } from './api/IContainerOptions';
 import { Container } from './container';
 import { resolveAndLoadModules } from './moduleLoader';
+import { loadConfig } from './containerConfig';
 
 /**
  * Factory to create Container object
  */
 export class ContainerFactory {
+  private static isConfigLoaded: boolean = false;
+
   /**
    * Main function to create a new Container object with provided options
    * @param options Provided options. See {@link IContainerOptions}
    */
   static async create(options?: StandaloneContainerOptions): Promise<Container> {
-    await resolveAndLoadModules(options);
+    if (!this.isConfigLoaded) await loadConfig();
+    await resolveAndLoadModules(options ?? 'default');
 
     return new Container();
   }
--- src/moduleLoader.ts
+++ src/moduleLoader.ts
@@ -1,15 +1,16 @@
-import { IContainerOptions } from './api/IContainerOptions';
+import { IContainerOptions, StandaloneContainerOptions } from './api/IContainerOptions';
 import path from 'node:path';
 import fs from 'node:fs';
 import dynamicImport from './dynamicImport';
+import { getConfig } from './containerConfig';
 
 const loadModules = (options: IContainerOptions) => {
   options.catalogs!.forEach((catalog) => {
     const directory = options.baseDir;
 
-    const catalogPath = path.resolve(directory, catalog);
+    const catalogPath = path.resolve(directory, catalog.path);
     const files = fs
-      .readdirSync(catalogPath, { withFileTypes: true, recursive: options.recursive })
+      .readdirSync(catalogPath, { withFileTypes: true, recursive: catalog.recursive })
       .filter((file) => file.isFile());
 
     files.forEach(async (file) => {
@@ -23,16 +24,13 @@ export const resolveAndLoadModules = async (options?: StandaloneContainerOptions
     if (options?.catalogs) {
       loadModules(options);
     }
+    return;
   }
+  const containerConfig = getConfig(options);
   const root = process.cwd();
-  if (root) {
-    const configFile = path.join(root, 'lazyinject.config.js');
-    if (!fs.existsSync(configFile)) return;
-    const lazyConfig = await dynamicImport(configFile);
-    const newOptions = {
-      baseDir: root,
-      catalogs: lazyConfig.catalogs,
-    };
-    loadModules(newOptions);
-  }
+  const newOptions: StandaloneContainerOptions = {
+    baseDir: root,
+    catalogs: containerConfig.catalogs,
+  };
+  loadModules(newOptions);
 };
