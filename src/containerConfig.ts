24b37b1c0c4367da09b76724d54514e051d7cc9f
Duude92 <switch4acc@gmail.com>
2025-07-18 14:45:46 +0300

    Added containerConfig.ts, which should load config from lazyinject.config.js config file and retrieve configuration per container

---
+++ src/containerConfig.ts
@@ -0,0 +1,20 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import dynamicImport from './dynamicImport';
+import { Config as LazyInjectContainerOptions } from './lazyinject.config';
+
+let config: LazyInjectContainerOptions;
+export const getConfig = (containerId: string | 'default') => {
+  if (config[containerId] == undefined) throw new Error(`Container id ${containerId} not found`);
+  return config[containerId];
+};
+
+export const loadConfig = async () => {
+  const root = process.cwd();
+  if (root) {
+    const configFile = path.join(root, 'lazyinject.config.js');
+    if (!fs.existsSync(configFile)) return;
+    const lazyConfig = await dynamicImport(configFile);
+    config = lazyConfig.default;
+  }
+};
