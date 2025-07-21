fb9863f74bcd9478fbd9bbaa8f33ead9d47c603e
Duude92 <switch4acc@gmail.com>
2025-07-21 16:35:51 +0300

    Replace ContainerRegistryStatic with this.registry

--- src/container.ts
+++ src/container.ts
@@ -1,24 +1,31 @@
 import { InterfaceType } from './api/interfaceType';
-import { ContainerRegistryStatic } from './containerRegistry';
+import { ContainerRegistry } from './containerRegistry';
 
 /**
  * Container class
  */
 export class Container {
+  constructor(private registry: ContainerRegistry) {}
+
+  get ContainerRegistry(): ContainerRegistry {
+    return this.registry;
+  }
+
   /**
    * Retrieves given type object from container registry
    * @param type Object type to retrieve to
    */
   get<T>(type: InterfaceType): T | undefined {
-    const lazyObject = ContainerRegistryStatic.getExport<T>(type);
+    const lazyObject = this.registry.getExport<T>(type);
     return lazyObject?.Value;
   }
+
   /**
    * Retrieves given type objects array from container registry
    * @param type Object type to retrieve to
    */
   getMany<T>(type: InterfaceType): T[] | undefined {
-    const lazyObject = ContainerRegistryStatic.getMany<T>(type);
-    return lazyObject?.map(x=>x.Value as T) ?? [];
+    const lazyObject = this.registry.getMany<T>(type);
+    return lazyObject?.map((x) => x.Value as T) ?? [];
   }
 }
