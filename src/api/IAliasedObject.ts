6de0f6b9880ba27a11ecf834e01ab687c30412ab
Duude92 <switch4acc@gmail.com>
2026-01-16 11:58:48 +0200

    feat: add alias support

---
+++ src/api/IAliasedObject.ts
@@ -0,0 +1,26 @@
+import { Lazy } from '../lazy.js';
+
+export interface IAliasedObject {
+  alias: string;
+  objects: Lazy<unknown>[];
+  child?: IAliasedObject[];
+}
+export const isAliasedObject = (obj: unknown): obj is IAliasedObject => {
+  return obj !== null && typeof obj === 'object' && 'alias' in obj && 'objects' in obj;
+};
+export const makeAlias = (aliases: string[], obj: Lazy<unknown>): IAliasedObject => {
+  return {
+    alias: aliases.pop()!,
+    objects: [obj],
+    child: aliases.length > 0 ? [makeAlias(aliases, obj)] : undefined,
+  };
+};
+export const getAliasedObjects = (path: string[], parent: IAliasedObject): IAliasedObject | undefined => {
+    const alias = path.pop()!;
+    const obj = parent.child?.find((x) => x.alias === alias);
+    if (!obj) return;
+    if (path.length < 1) {
+      return obj;
+    }
+    return getAliasedObjects(path, obj);
+  }
--- src/containerRegistry.ts
+++ src/containerRegistry.ts
@@ -3,13 +3,30 @@ import { Lazy } from './lazy';
 import { ExportedType } from './api/ConstructorType';
 import { IImportedType } from './api/IImportedType';
 import { IContainerRegistryImportOptions } from './api/IContainerRegistryImportOptions';
+import { getAliasedObjects, IAliasedObject, makeAlias } from './api/IAliasedObject';
 
 /**
  * Container object which stores imported and exported objects|constructor functions
  */
 class ContainerRegistry {
-  private exportedMap = new Map<InterfaceType, Lazy<unknown>[]>();
+  private exportedMap = new Map<InterfaceType, IAliasedObject>();
   private importedMap = new Map<InterfaceType, IImportedType[]>();
+
+  private addToAlias(aliases: string[], obj: Lazy<unknown>, parent: IAliasedObject) {
+    parent.objects.push(obj);
+    if (aliases.length < 1) return;
+    if (parent.child) {
+      const child = parent.child.find((x) => x.alias === aliases[0]);
+      if (child) {
+        aliases.shift();
+        this.addToAlias(aliases, obj, child);
+        return;
+      }
+      parent.child.push(makeAlias(aliases, obj));
+      return;
+    }
+    parent.child = [makeAlias(aliases, obj)];
+  }
 
   constructor() {}
 
@@ -19,12 +36,24 @@ class ContainerRegistry {
    * @param exportedObject Constructor function or object that needs to be resolved later
    */
   setExport(type: InterfaceType, exportedObject: ExportedType): void {
-    let array = this.exportedMap.get(type);
-    if (!array) {
-      array = [];
-      this.exportedMap.set(type, array);
+    if (typeof type !== 'string') {
+      let array = this.exportedMap.get(type);
+      if (!array) {
+        array = { alias: type.toString(), objects: [] };
+        this.exportedMap.set(type, array);
+      }
+      array.objects.push(new Lazy(exportedObject));
+      return;
     }
-    array.push(new Lazy(exportedObject));
+    const path = type.split('.');
+    const lazyObject = new Lazy(exportedObject);
+    const alias = path.shift()!;
+    const aliasedObject = this.exportedMap.get(alias);
+    if (!aliasedObject) {
+      this.exportedMap.set(alias, makeAlias(path, lazyObject));
+      return;
+    }
+    this.addToAlias(path, lazyObject, aliasedObject);
   }
 
   /**
@@ -32,15 +61,24 @@ class ContainerRegistry {
    * @param type Type of object or constructor function to retrieve to
    */
   getExport<T>(type: InterfaceType): Lazy<T> | undefined {
+    if (typeof type === 'string') {
+      const path = type.split('.');
+      if (path.length > 1) {
+        const alias = path.shift()!;
+        const aliasedObject = this.exportedMap.get(alias);
+        if (!aliasedObject) return;
+        return getAliasedObjects(path, aliasedObject)?.objects[0] as Lazy<T>;
+      }
+    }
     const directGet = this.exportedMap.get(type);
     if (directGet) {
-      return directGet[0] as Lazy<T>;
+      return directGet.objects[0] as Lazy<T>;
     }
     if (typeof type === 'function') {
-      return this.exportedMap.get(type.name)![0] as Lazy<T>;
+      return this.exportedMap.get(type.name)?.objects[0] as Lazy<T>;
     }
     if (typeof type === 'symbol') {
-      return this.exportedMap.get(type.description!)![0] as Lazy<T>;
+      return this.exportedMap.get(type.description!)?.objects[0] as Lazy<T>;
     }
   }
 
@@ -49,15 +87,24 @@ class ContainerRegistry {
    * @param type Type of object or constructor function to retrieve to
    */
   getMany<T>(type: InterfaceType): Lazy<T>[] | undefined {
+    if (typeof type === 'string') {
+      const path = type.split('.');
+      if (path.length > 1) {
+        const alias = path.shift()!;
+        const aliasedObject = this.exportedMap.get(alias);
+        if (!aliasedObject) return;
+        return getAliasedObjects(path, aliasedObject)?.objects as Lazy<T>[];
+      }
+    }
     const directGet = this.exportedMap.get(type);
     if (directGet) {
-      return directGet as Lazy<T>[];
+      return directGet.objects as Lazy<T>[];
     }
     if (typeof type === 'function') {
-      return this.exportedMap.get(type.name) as Lazy<T>[];
+      return this.exportedMap.get(type.name)?.objects as Lazy<T>[];
     }
     if (typeof type === 'symbol') {
-      return this.exportedMap.get(type.description!) as Lazy<T>[];
+      return this.exportedMap.get(type.description!)?.objects as Lazy<T>[];
     }
   }
 
@@ -72,7 +119,7 @@ class ContainerRegistry {
     ctorType: InterfaceType,
     importType: InterfaceType,
     parameterIndex: number,
-    options: IContainerRegistryImportOptions,
+    options: IContainerRegistryImportOptions
   ): void {
     const single = options.single ?? true;
     if (!this.importedMap.has(ctorType)) {
